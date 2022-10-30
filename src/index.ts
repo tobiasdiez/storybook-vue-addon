d4f1b185a348cdb0b091284bb32e73610053f936
Tobias Diez <code@tobiasdiez.com>
2022-10-30 11:33:37 +0000

    feat: support vue plugin in pipeline

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,37 @@ import { createUnplugin } from 'unplugin'
 import { transform as transformVue } from './core/transform'
 import type { Options } from './types'
 
+// We a custom type so that the vue plugin is ignoring the "main" import
+const STORIES_INTERNAL_SUFFIX = '?vue&type=stories'
+const STORIES_PUBLIC_SUFFIX = '.stories.vue'
+
 export default createUnplugin<Options>(_options => ({
   name: 'unplugin-starter',
   enforce: 'pre',
+  async resolveId(source, importer, options) {
+    if (source.endsWith(STORIES_PUBLIC_SUFFIX)) {
+      // Determine what the actual entry would have been. We need "skipSelf"
+      // to avoid an infinite loop.
+      const resolution = await this.resolve(source, importer, { skipSelf: true, ...options })
+
+      // If it cannot be resolved or is external, just return it so that
+      // Rollup can display an error
+      if (!resolution || resolution.external)
+        return resolution
+
+      console.log('resolveIdStory', resolution)
+      return {
+        ...resolution,
+        id: resolution.id + STORIES_INTERNAL_SUFFIX,
+      }
+    }
+  },
   transformInclude(id) {
-    return id.endsWith('.stories.vue')
+    console.log('transformInclude', id)
+    return id.endsWith(STORIES_INTERNAL_SUFFIX)
   },
   transform(code) {
+    console.log('transform', code)
     return transformVue(code)
   },
 }))

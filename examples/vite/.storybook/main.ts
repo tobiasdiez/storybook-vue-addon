1323b90b7515c01645c7e1795784221cca22752f
Tobias Diez <code@tobiasdiez.de>
2023-03-07 21:29:59 +0800

    chore: upgrade storybook to v7

--- examples/vite/.storybook/main.ts
+++ examples/vite/.storybook/main.ts
@@ -1,19 +1,15 @@
 import Inspect from 'vite-plugin-inspect'
-
 export default {
-  stories: [
-    '../src/**/*.stories.mdx',
-    '../src/**/*.stories.@(js|jsx|ts|tsx|vue)',
-  ],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|vue)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '../../../src/storybook.ts',
   ],
-  framework: '@storybook/vue3',
-  core: {
-    builder: '@storybook/builder-vite',
+  framework: {
+    name: '@storybook/vue3-vite',
+    options: {},
   },
   features: {
     storyStoreV7: false,
@@ -21,7 +17,6 @@ export default {
   async viteFinal(config, { configType }) {
     // Inspect result can be found at subroute '/__inspect', e.g. http://127.0.0.1:6006/__inspect/
     config.plugins.unshift(Inspect())
-
     return config
   },
 }

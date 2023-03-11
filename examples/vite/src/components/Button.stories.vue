851ec8201cdf308f8870431c002fc46576474ab1
Tobias Diez <code@tobiasdiez.de>
2023-03-11 22:38:33 +0800

    fix linter

--- examples/vite/src/components/Button.stories.vue
+++ examples/vite/src/components/Button.stories.vue
@@ -3,7 +3,10 @@ import MyButton from './Button.vue'
 </script>
 
 <template>
-  <Stories :component="MyButton" title="Components/Button">
+  <Stories
+    :component="MyButton"
+    title="Components/Button"
+  >
     <Story title="Primary">
       <MyButton label="Button" />
     </Story>

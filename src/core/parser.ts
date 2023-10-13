9b5303feda99a51fd5142066c5eff343a60a4b78
Tobias Diez <code@tobiasdiez.de>
2023-10-13 21:53:04 +0800

    fix nodetype

--- src/core/parser.ts
+++ src/core/parser.ts
@@ -1,5 +1,4 @@
 import type { ElementNode } from '@vue/compiler-core'
-import { NodeTypes } from '@vue/compiler-core'
 import type { SFCDescriptor } from 'vue/compiler-sfc'
 import {
   compileScript,
@@ -8,6 +7,38 @@ import {
 } from 'vue/compiler-sfc'
 
 import { sanitize } from '@storybook/csf'
+
+// import { NodeTypes } from '@vue/compiler-core'
+// Doesn't work, for some reason, maybe https://github.com/vuejs/core/issues/1228
+enum NodeTypes {
+  ROOT = 0,
+  ELEMENT = 1,
+  TEXT = 2,
+  COMMENT = 3,
+  SIMPLE_EXPRESSION = 4,
+  INTERPOLATION = 5,
+  ATTRIBUTE = 6,
+  DIRECTIVE = 7,
+  COMPOUND_EXPRESSION = 8,
+  IF = 9,
+  IF_BRANCH = 10,
+  FOR = 11,
+  TEXT_CALL = 12,
+  VNODE_CALL = 13,
+  JS_CALL_EXPRESSION = 14,
+  JS_OBJECT_EXPRESSION = 15,
+  JS_PROPERTY = 16,
+  JS_ARRAY_EXPRESSION = 17,
+  JS_FUNCTION_EXPRESSION = 18,
+  JS_CONDITIONAL_EXPRESSION = 19,
+  JS_CACHE_EXPRESSION = 20,
+  JS_BLOCK_STATEMENT = 21,
+  JS_TEMPLATE_LITERAL = 22,
+  JS_IF_STATEMENT = 23,
+  JS_ASSIGNMENT_EXPRESSION = 24,
+  JS_SEQUENCE_EXPRESSION = 25,
+  JS_RETURN_STATEMENT = 26,
+}
 
 export interface ParsedMeta {
   title?: string

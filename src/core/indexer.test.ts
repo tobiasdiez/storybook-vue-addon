dc67f60c41e8cedf4591b7b3af0023a757ee08cc
Tobias Diez <code@tobiasdiez.de>
2024-08-04 19:26:48 +0200

    update tests

--- src/core/indexer.test.ts
+++ src/core/indexer.test.ts
@@ -4,24 +4,23 @@ import { describe, expect, it } from 'vitest'
 describe('indexer', () => {
   const options = {
     makeTitle: (userTitle: string | undefined) => userTitle || 'UNDEFINED',
+    fileName: 'file.vue',
   }
   it('handles one simple story', () => {
     const code =
       '<template><Stories title="Simple"><Story title="Primary">hello</Story></Stories></template>'
     const result = indexerCode(code, options)
     expect(result).toMatchInlineSnapshot(`
-      {
-        "meta": {
+      [
+        {
+          "exportName": "primary",
+          "importPath": "file.vue",
+          "name": "Primary",
           "tags": [],
           "title": "Simple",
+          "type": "story",
         },
-        "stories": [
-          {
-            "id": "simple--primary",
-            "name": "Primary",
-          },
-        ],
-      }
+      ]
     `)
   })
 
@@ -30,42 +29,43 @@ describe('indexer', () => {
       '<template><Stories title="Simple"><Story title="바보 (babo) 2:3!">hello</Story</template>'
     const result = indexerCode(code, options)
     expect(result).toMatchInlineSnapshot(`
-      {
-        "meta": {
+      [
+        {
+          "exportName": "___babo_2_3",
+          "importPath": "file.vue",
+          "name": "바보 (babo) 2:3!",
           "tags": [],
           "title": "Simple",
+          "type": "story",
         },
-        "stories": [
-          {
-            "id": "simple--babo-2-3",
-            "name": "바보 (babo) 2:3!",
-          },
-        ],
-      }
+      ]
     `)
   })
 
   it('creates unique ids for stories with same title', () => {
+    // TODO: This is currently not working as expected
     const code =
       '<template><Stories title="Simple"><Story title="Primary">hello</Story><Story title="Primary">hello two</Story></Stories></template>'
     const result = indexerCode(code, options)
     expect(result).toMatchInlineSnapshot(`
-      {
-        "meta": {
+      [
+        {
+          "exportName": "primary",
+          "importPath": "file.vue",
+          "name": "Primary",
           "tags": [],
           "title": "Simple",
+          "type": "story",
         },
-        "stories": [
-          {
-            "id": "simple--primary",
-            "name": "Primary",
-          },
-          {
-            "id": "simple--primary",
-            "name": "Primary",
-          },
-        ],
-      }
+        {
+          "exportName": "primary",
+          "importPath": "file.vue",
+          "name": "Primary",
+          "tags": [],
+          "title": "Simple",
+          "type": "story",
+        },
+      ]
     `)
   })
 })
